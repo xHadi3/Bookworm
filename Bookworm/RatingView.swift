455b2a8ed04653074824329c3c48d7f63c56a7e0
xHadi3 <hadiessam9@gmail.com>
2025-02-18 02:03:16 +0300

    making the ratingView(rating widget)

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  ratingView.swift
+//  Bookworm
+//
+//  Created by Hadi Al zayer on 18/08/1446 AH.
+//
+
+import SwiftUI
+
+struct ratingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ratingView()
+}
