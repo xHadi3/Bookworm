c8baf7eee91eba4550430f273608013493bc89f7
xHadi3 <hadiessam9@gmail.com>
2025-02-18 18:41:03 +0300

    preparing the detail page for the books

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Hadi Al zayer on 19/08/1446 AH.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
